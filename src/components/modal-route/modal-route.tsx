dd9eea2da13ef76d1e4bc1ae2593707966aac2d0
Dmitriy <mocdmi@yandex.ru>
2025-12-15 01:55:23 +0300

    style: add todo for modal route

--- src/components/modal-route/modal-route.tsx
+++ src/components/modal-route/modal-route.tsx
@@ -5,6 +5,7 @@ import { Outlet, useNavigate, useParams, useSearchParams } from 'react-router-do
 
 import { useModalActions } from '../modal/hooks/use-modal-actions';
 
+// Переделать, убрать search параметры и сделать универсальным
 export const ModalRoute = (): React.JSX.Element | null => {
   const { id } = useParams();
   const { data } = useGetAllIngredientsQuery();
